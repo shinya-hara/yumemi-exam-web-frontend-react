25a6535dfd4496eef405386891e31af860823547
Shinya Hara <s.hara@sonicmoov.com>
2022-05-19 01:16:15 +0900

    chore: Setup eslint & prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'css',
+};
---
+++ src/pages/Index.tsx
@@ -0,0 +1,7 @@
+export const PageIndex = () => {
+  return (
+    <div>
+      <h1>INDEX</h1>
+    </div>
+  );
+};
